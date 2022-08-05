8b79d7dba0dc9ed2808ffa4c73ac5404926b2940
andrewKievUA <andrew1990mk@gmail.com>
2022-08-05 10:15:11 +0300

    orders reworked

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -11,115 +11,177 @@ const config = require("config");
 
 //andrew1990mk@gmail.com
 router.post("/AddOrder", async (req, res, next) => {
-  const { email, password, cartNumber, name } = req.body;
-  try {
-    console.log(req.body);
-
-    User.updateOne(
-      { _id: req.body.userId },
-      {
-        $push: {
-          orders: new Order({
-            instaLinkCustomer: req.body.instaLinkCustomer,
-            instaLinkGoods: req.body.instaLinkGoods,
-            arrived: req.body.arrived,
-            payed: req.body.payed,
-            comment: req.body.comment,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            city: req.body.city,
-            telephone: req.body.telephone,
-            postNumer: req.body.postNumer,
-            userId: req.body.userId,
-          }),
-        },
-      },
-
-      function (err, res) {
-        console.log(err, res);
+  let checkParams = () => {
+    if (
+      req.body.userId &&
+      req.body.firstName &&
+      req.body.instaLinkGoods &&
+      req.body.lastName
+    ) {
+      if (req.body.userId.length == 24) {
+        console.log("permited");
+        return true;
       }
-    );
+    }
+  };
 
-    res.status(201).json({ message: "Заказ создан" });
-    //sendEmail(email||`andrew1990mk@gmail.com`,"Приветствуем на Insta saver",`пользователь создан ${email} вам необходимо подтвердить ваш емейл http://localhost:3000/Register/Confirm:44`)
-  } catch (e) {
-    res.status(500).json({ message: "errror happends" });
-    console.log(e);
+  if (checkParams()) {
+    try {
+      console.log(req.body);
+      const order = new Order({
+          instaLinkCustomer: req.body.instaLinkCustomer,
+          instaLinkGoods: req.body.instaLinkGoods,
+          arrived: req.body.arrived,
+          payed: req.body.payed,
+          comment: req.body.comment,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          city: req.body.city,
+          telephone: req.body.telephone,
+          postNumer: req.body.postNumer,
+          userId: req.body.userId, 
+      }
+      )
+      order.save()
+
+      res.status(201).json({ message: "Заказ создан" });
+    } catch (e) {
+      res.status(500).json({ message: "errror happends" });
+      console.log(e);
+    }
   }
 });
 
 router.post("/deleteOrder", async (req, res, next) => {
   console.log(req.body);
-  const { id, userId, instaLinkCustomer, instaLinkGoods } = req.body;
-  try {
-    // User.deleteOne({_id: userId},  function(err, res) {
-    //   console.log(err, res);
-    // } )
 
-    // arrayFilters?: { [key: string]: any }[];
-    // {instaLinkCustomer,instaLinkGoods}
-    console.log(instaLinkGoods, userId, id);
-    User.updateOne(
-      //delete
-      { _id: userId },
-      {
-        $pull: { orders: { _id: Types.ObjectId(id) } },
-      },
-      { multi: true, strictQuery: true, new: true },
-
-      function (err, res) {
-        console.log(err, res);
+  let checkParams = () => {
+    if (req.body._id && req.body.id && req.body.instaLinkGoods) {
+      if (req.body._id.length == 24) {
+        return true;
       }
-    );
+    }
+  };
 
-    res.status(201).json({ message: "Заказ создан" });
-    //sendEmail(email||`andrew1990mk@gmail.com`,"Приветствуем на Insta saver",`пользователь создан ${email} вам необходимо подтвердить ваш емейл http://localhost:3000/Register/Confirm:44`)
-  } catch (e) {
-    res.status(500).json({ message: "errror happends" });
-    console.log(e);
+  if (checkParams()) {
+    const { id, userId, instaLinkCustomer, instaLinkGoods } = req.body;
+    try {
+      console.log(instaLinkGoods, userId, id);
+      User.updateOne(
+        { _id: userId },
+        {
+          $pull: { orders: { _id: Types.ObjectId(id) } },
+        },
+        { multi: true, strictQuery: true, new: true },
+
+        function (err, res) {
+          console.log(err, res);
+        }
+      );
+
+      res.status(201).json({ message: "Заказ создан" });
+    } catch (e) {
+      res.status(500).json({ message: "errror happends" });
+      console.log(e);
+    }
   }
 });
 
+
+
+
+
+router.post("/updateOrder", async (req, res, next) => {
+  console.log("aaa")
+  let checkParams = () => {
+    if (req.body.userId && req.body.instaLinkGoods) {
+      if (req.body.userId.length == 24) {
+        return true;
+      }
+    }
+  };
+
+  if (checkParams()) {
+    const { id, userId, instaLinkCustomer, instaLinkGoods } = req.body;
+    try {
+      ///console.log(instaLinkGoods, userId, id);
+      console.log(userId, ' ', req.body._id)
+      let _id = req.body._id
+
+      let z = req.body
+      console.log(z)
+      
+
+     let user = await Order.updateOne({userId,_id},{
+      instaLinkCustomer: req.body.instaLinkCustomer,
+      instaLinkGoods: req.body.instaLinkGoods,
+      arrived: req.body.arrived,
+      payed: req.body.payed,
+      comment: req.body.comment,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      city: req.body.city,
+      telephone: req.body.telephone,
+      postNumer: req.body.postNumer,
+
+  }
+     ) 
+
+      console.log(user)
+      res.status(201).json({ message: "Заказ создан" });
+    } catch (e) {
+      res.status(500).json({ message: "errror happends" });
+      console.log(e);
+    }
+  }
+});
+
+
+
 router.post("/showOrder", async (req, res, next) => {
-  // const { email, password, cartNumber, name } = req.body;
-  const orders = await User.findOne({ _id: req.body.userId });
-  console.log(orders.orders);
-  try {
-    res.status(201).json({ message: orders.orders });
-  } catch (e) {
-    res.status(500).json({ message: "errror happends" });
-    console.log(e);
+  let checkParams = () => {
+    if (req.body.userId) {
+      if (req.body.userId.length == 24) {
+        return true;
+      }
+    }else{console.log("blocked",req.body);}
+  };
+
+  if (checkParams()) {
+    const orders = await Order.find({ userId: req.body.userId });
+    console.log(orders,"show order");
+    try {
+      res.status(201).json({ message: orders });
+    } catch (e) {
+      res.status(500).json({ message: "errror happends" });
+      console.log(e);
+    }
   }
 });
 
 router.post("/detailOrder", async (req, res, next) => {
   // const { email, password, cartNumber, name } = req.body;
-
-  console.log("body");
-  console.log(req.body, "body");
-  const page = await User.findOne(
-    { _id: req.body.userId },
-    { orders: { $elemMatch: { _id: Types.ObjectId(req.body._id) } } }
-  );
-  console.log(page);
-  try {
-    res.status(201).json({ message: page });
-  } catch (e) {
-    res.status(500).json({ message: "errror happends" });
-    console.log(e);
+  let checkParams = () => {
+    if (req.body._id && req.body.userId) {
+      if (req.body._id.length == 24 && req.body.userId.length == 24) {
+        return true;
+      }
+    }
+  };
+  if (checkParams()) {
+    console.log(req.body, "body");
+    const page = await Order.findById(req.body._id)
+    console.log(page,"detailOrder");
+    try {
+      res.status(201).json({ message: page });
+    } catch (e) {
+      res.status(500).json({ message: "errror happends" });
+      console.log(e);
+    }
+  } else {
+    console.log("asdfasdf");
+    res.status(200).json({ message: "validation failed" });
   }
 });
 
 module.exports = router;
-
-// User.updateOne(  //delete
-// { _id: `62e3be3549cae9d52f0ff28f` },
-// {
-//   $pull: { orders: { instaLinkGoods: "ddddddddddddd" } }},
-//   { "multi" : true }  ,
-
-//   function(err, res) {
-//     console.log(err, res);
-//   },
-
-// );
